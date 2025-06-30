bacb4c0bf43da7effc89daf5f5dcbef9d32955f2
noyceliu <hyliu@jlsemi.com>
2025-06-30 15:07:37 +0800

    change deploy method

--- js/6312ba2c.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  fixit.decryptor.addEventListener('decrypted', function() {
-    console.log('after password decryption')
-  })
-  if (window.localStorage.getItem(`fixit-decryptor/#${location.pathname}`)) {
-    console.log('after automatic decryption')
-  }
-});
